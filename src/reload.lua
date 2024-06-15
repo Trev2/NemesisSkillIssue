9e3b180ad0d28f89be4dafa1f6220642e2b2ccae
TrevTheModder <trevermckenzie3@gmail.com>
2024-06-15 15:10:56 -0400

    Update reload.lua

--- src/reload.lua
+++ src/reload.lua
@@ -22,3 +22,18 @@ end
 function trigger_Gift()
 	modutil.mod.Hades.PrintOverhead(config.message)
 end
+
+function CheckShoppingEventThread_Wrap(base, eventSource, args)
+  if ShoppingNPCId then
+    local delay = 3600
+  if RandomChance( eventData.InstantChance ) then
+      delay = 3600
+      instantPurchase = false
+  elseif RandomChance( eventData.NeverChance ) then
+      delay = 3600
+  end
+  end
+end
+
+NPCVariantData.NPC_Nemesis_01.ShopEventData.InstantChance = 0
+NPCVariantData.NPC_Nemesis_01.ShopEventData.NeverChance = 1
